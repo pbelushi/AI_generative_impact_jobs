d70468f7ae8833074ac6d8afc5273fed07b73a5c
pbelushi <paulo@commcloud.com.br>
2026-03-25 15:46:09 -0300

    update path in scripts

--- script/estimativa_impacto.py
+++ script/estimativa_impacto.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 # 1. Carregar a sua planilha de classificação de IA
-df_classificacao = pd.read_csv(r"../Data/CBOs por exposição à IA generativa - Página1 (1).csv")
+df_classificacao = pd.read_csv(r"Data/CBOs por exposição à IA generativa - Página1 (1).csv")
 
 # 2. Carregar o arquivo exportado da Base dos Dados referente a 2024
-df_rais = pd.read_csv(r"../Data/vinculos_por_cbo_2024.csv")
+df_rais = pd.read_csv(r"Data/vinculos_por_cbo_2024.csv")
 
 # Converter a coluna de CBO para texto (string) para garantir o cruzamento perfeito
 df_classificacao['Código CBO'] = df_classificacao['Código CBO'].astype(str)
@@ -30,8 +30,8 @@ total_mapeado = resumo_impacto['total_vinculos_ativos'].sum()
 resumo_impacto['percentual (%)'] = (resumo_impacto['total_vinculos_ativos'] / total_mapeado) * 100
 
 # 5. SALVAR OS ARQUIVOS (com números crus e formatação amigável para Excel BR)
-caminho_detalhado = r"../Data/Estimativa_Detalhada_Impacto_IA_2024.csv"
-caminho_resumo = r"../Data/Resumo_Impacto_IA_2024.csv"
+caminho_detalhado = r"Data/Estimativa_Detalhada_Impacto_IA_2024.csv"
+caminho_resumo = r"Data/Resumo_Impacto_IA_2024.csv"
 
 df_impacto.to_csv(caminho_detalhado, index=False, sep=';', encoding='utf-8-sig')
 resumo_impacto.to_csv(caminho_resumo, index=False, sep=';', encoding='utf-8-sig')
@@ -61,7 +61,7 @@ colunas_para_salvar = ['Gradiente', 'Código CBO', 'total_vinculos_ativos']
 top_10_limpo = top_10_por_gradiente[colunas_para_salvar]
 
 # 8. Salvar esse novo recorte em um CSV configurado para o Excel
-caminho_top10 = r"../Data/Top_10_CBOs_por_Gradiente.csv"
+caminho_top10 = r"Data/Top_10_CBOs_por_Gradiente.csv"
 top_10_limpo.to_csv(caminho_top10, index=False, sep=';', encoding='utf-8-sig')
 
 print(f"✅ Arquivo com o Top 10 Ocupações por Gradiente salvo com sucesso em: {caminho_top10}")
